90bf9ba3a4f547ad289feb5d9c55c9f3f73c8b36
Kennedy, Trevor <tkennedy@bethel.jw.org>
2020-03-19 16:23:19 -0400

    initial flutter app creation

---
+++ android/app/src/main/kotlin/com/hsco/relay/relay/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hsco.relay.relay
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
